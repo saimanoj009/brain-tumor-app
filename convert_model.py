513c0f84a68d6ef27d2229b0b39259d9613fa207
saimanoj009 <saimanoj0914@gmail.com>
2026-04-06 14:16:52 +0530

    Delete convert_model.py

--- convert_model.py
+++
@@ -1,9 +0,0 @@
-from tensorflow.keras.models import load_model
-
-# Load existing model
-model = load_model("brain_tumor_model.keras", compile=False)
-
-# Save as .h5
-model.save("brain_tumor_model.h5")
-
-print("✅ Model converted successfully!")
